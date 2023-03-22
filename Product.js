1122ae4330cbe02b91074da9ab9aeb3b65e7adee
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-03-22 18:38:42 +0530

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,8 +1,45 @@
+import axios from "axios";
+import { useState,useEffect } from "react";
+
 function Product(){
+
+    const[product, setproduct ]=useState();
+useEffect(()=>{
+    const Handleproduct = async(e) =>{
+        const res = await axios.get("http://localhost:8006/product");
+        console.log(res.data)
+        setproduct(res.data)
+        };
+        Handleproduct()
+},[]);
+   
     return(
         <>
+
         <div>
-            This is a product screen</div></>
+        <table>
+        <th>Productcode</th>
+                <th>Productname</th>
+                <th>Brand</th>
+                <th>Productprice</th>
+                <th>Productstatus</th>
+            {product&&product.map((items)=>(<>
+    
+                <tr>
+                    <td>{items.productcode}</td>
+                    <td>{items.productname}</td>
+                    <td>{items.brand}</td>
+                    <td>{items.productprice}</td>
+                    <td>{items.productstatus}</td>
+                </tr>
+               
+
+            
+            </>))}
+            </table>
+           
+        </div>
+        </>
     )
 }
 
