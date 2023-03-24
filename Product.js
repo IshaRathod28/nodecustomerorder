47fc380004c8f23fe868c1963c76c32e39c2961f
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-03-24 18:41:09 +0530

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+// const orderid = require('order-id')('key');
 
 // var y=[];
 function Product() {
   //   const [adddata, setadddata] = useState([]);
+  // const id = orderid.generate();
+  // orderid.getTime(id);
+  // console.log(id);
   const [product, setproduct] = useState();
   var [product1, setproduct1] = useState([]);
   var [quantity, setquantity] = useState(1);
-
+  var [orddate,setorddate] =useState("")
+  var[price,totalprice]=useState("")
+var[order,setorder]=useState("")
   useEffect(() => {
     const Handleproduct = async (e) => {
       const res = await axios.get("http://localhost:8006/product");
@@ -15,10 +21,63 @@ function Product() {
       setproduct(res.data);
       //   setadddata(res.data);
     };
+    
     Handleproduct();
   }, []);
 
-  console.log(quantity);
+ 
+
+  const Grandtotal = async(e)=>{
+    const res = await axios.post("http://localhost:8006/grandtotal",{
+      product1:product1
+    })
+  }
+ 
+  const managedatainordertable =async(e)=>{
+    const res=await axios.post("http://localhost:8006/managedatainordertable")
+    setproduct1([])
+  };
+
+  const totalpriceoforder = async(e) => {
+    const res = await axios.get("http://localhost:8006/totalpriceoforder")
+   console.log(res.data[0]) 
+   console.log("true")
+   document.getElementById("grandtotal").innerHTML=res.data[0].grandtotal;
+   console.log("true")
+  }
+  
+  
+const displayorderlisting = async(e) => {
+  const res = await axios.get("http://localhost:8006/displayorderlisting")
+  console.log(res.data)
+  setorder(res.data)
+  console.log(order)
+}
+
+
+  const orderdatetable = async(e) => {
+   
+    var orddate = new Date();
+    console.log(orddate.getDate());
+    console.log(orddate.getMonth()+1);
+    console.log(orddate.getFullYear());
+ 
+    var day = orddate.getDate();
+    var month = orddate.getMonth()+1;
+    var year =orddate.getFullYear();
+ 
+    var orderdate= day + "/" + month + "/" + year;
+    console.log(orderdate)
+    setorddate(orderdate)
+   
+    const res = await axios.post("http://localhost:8006/orderdatetable",{
+      orddate:orddate
+    })
+  }
+
+ console.log(orddate)
+  // console.log(quantity);
+  // console.log(product1)
 
   // const faltu = ()=>{
   //     // console.log(adddata)
@@ -84,7 +143,7 @@ function Product() {
                         var storeselectedproductdata = items;
                         //add1=>upervala ma product 1 no data store thase
                         // console.log(x);
-
+// console.log(storeselectedproductdata)
                         //setproduct1([storeselectedproductdata])
                         //uper jevu kare to data select pan thay and show pan thay but ek j row ma natak thay , aapane particular product mate different row joiye etle alag rite set karavva padse
 
@@ -126,16 +185,26 @@ function Product() {
                   <td>{items.productstatus}</td>
                   <td>
                     <button
+                    class="btn btn-success"
                       onClick={() => {
+                        // console.log(items)
+                        // console.log(product1)
                         // console.log("hjew")
-                        console.log(items.quantity)
+                        
+                       
+                       
                         if (items.quantity >= 1) {
                           setquantity(quantity-1);
                           items.quantity -= 1 ; 
-                        // id(items.productcode,quantity-1)
+                       
+                           // id(items.productcode,quantity-1)
 
                         //   quantity -= 1;
+                        // console.log(items.productprice)
+                        // console.log(items.quantity)
 
+                        // setproductprice(items.productprice)
+                        // setproductqty(items.quantity)
                           // setAmount(theArray.pprice);
 
                           // setAmount(amount - theArray.pprice);
@@ -148,20 +217,101 @@ function Product() {
                     <button
                       onClick={()=> {
                         setquantity(quantity + 1);
+                        // console.log(product1)
+                        // console.log(items.productprice)
 
                         items.quantity += 1 ; 
-
+                        // console.log(items.productprice)
+                        // console.log(items.quantity)
                         // setAmount(theArray.pprice);
 
                         // setAmount(amount + theArray.pprice);
                       }}
+
+                      class="btn btn-success"
                     >
                       +
                     </button>
                   </td>
+                  <td>
+
+                    {items.productprice*items.quantity}
+
+                  </td>
                 </tr>
               </>
             ))}
+        </table>
+        <br />
+
+<div>
+        <button type="submit" onClick={Grandtotal} class="btn btn-danger">SUBMIT</button>
+        <button type="submit" onClick={managedatainordertable}  class="btn btn-danger">CLEAR</button>
+</div>
+<br />
+        <div>
+        <button onClick={totalpriceoforder}  class="btn btn-danger"> Click here to see the grandtotal</button>
+        <p id="grandtotal"></p>
+        </div>
+    
+      </div>
+<div>
+  <button class="btn btn-danger" onClick={orderdatetable}>
+    Click here to place the order
+  </button>
+  
+</div>
+<br />
+<div>
+<button class="btn btn-danger" onClick={displayorderlisting}>
+    Click here to see your order list
+  </button>
+</div>
+      <div>
+        <h1>
+          Order Listing
+        </h1>
+        <table>
+         
+            <th>Order ID</th>
+            <th>Date</th>
+            <th>Action</th>
+          {order&&order.map((items)=>(
+            <>
+            <tr>
+              <td>{items.orderid}</td>
+              <td>{items.date}</td>
+              <td><button>View</button></td>
+              </tr></>
+          )
+            
+)}
+          
+        </table>
+      </div>
+
+      <div>
+        <h1>
+          Order Detail view
+        </h1>
+        <table>
+          
+            <th>productcode</th>
+            <th>productname</th>
+            <th>productprice</th>
+            <th>quantity</th>
+            <th>totalprice</th>
+            {product1 &&
+            product1.map((items) => (
+              <>
+                <tr>
+                  <td>{items.productcode}</td>
+                <td>{items.productname}</td>
+                  <td>{items.productprice}</td>
+                <td>{items.quantity}</td>
+                <td>{items.productprice*items.quantity}</td>
+                  </tr></>))}
+         
         </table>
       </div>
     </>
