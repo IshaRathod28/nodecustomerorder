662614e1433b85dd224eeff2e5bcd0d35636458c
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-03-28 18:41:21 +0530

    Update Nav.js

--- Nav.js
+++ Nav.js
@@ -3,15 +3,33 @@ import {Link} from 'react-router-dom';
 function Nav(){
     return(
         <>
-          <h1>Dashboard</h1>
+        <div>
+        <div>
+        <h1 className='home'>Home Page</h1>
+        </div>
+        
         <div className='navdiv'>
-            <li><Link to="/registration" >Registration</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            {/* <li><Link to="/login">Profile page</Link></li> */}
-            <li><Link to="/product">Product Screen</Link></li>
-            <li><Link to="/order">Order Screen</Link></li>
-         
+          <table className='apptable'>
+            <tr className='apptable'>
             
+              <Link to="/registration" >Registration</Link>
+              </tr>
+              <tr className='apptable'>
+              
+              <Link to="/login">Login</Link>
+              </tr>
+              <tr className='apptable'>
+             
+              <Link to="/product">Product Screen</Link>
+              </tr>
+          </table>
+            <li></li>
+            <li></li>
+            {/* <li><Link to="/login">Profile page</Link></li> */}
+            <li></li>
+            {/* <li><Link to="/order">Order Screen</Link></li> */}
+         
+            </div>
             </div></>
     )
 }
