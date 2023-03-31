66f198f8d606111e7dc383c9afd0cdff355cbc4e
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-03-31 17:49:40 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,70 +1,3 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-const jwt=require('jsonwebtoken');
-app.use(cors());
-app.use(express.json());
-
-var mysql =require("mysql");
-var con=mysql.createConnection({
-    host:"192.168.2.8",
-    user:"trainee",
-    password:"trainee@123",
-    database:"trainee"
-})
-
-con.connect(function(error){
-    if(error) throw error;
-    console.log("Database is connected");
-});
-
-// const createtable=()=>{
-//     sql="CREATE TABLE isharegdata_cust ( recid bigint(20),customerid varchar(255),firstname VARCHAR(100), lastname VARCHAR(100) , email varchar(100),password varchar(255), mobile_number integer , gender varchar(50) , address varchar(255) , birthdate varchar(255) ,accesstoken varchar(255))"
-//     con.query(sql,(err,result)=>
-//     {
-//         if (err) throw err;
-//         console.log("your table is created");
-//     })
-// }
-
-// createtable()
-
-
-
-app.post("/regdata",(req,res)=>{
-    const customerid= req.body.customerid;
-    const firstname = req.body.firstname;
-    const lastname=req.body.lastname;
-    const email=req.body.email;
-    const password=req.body.password;
-const mobile_number=req.body.mobile_number;
-const gender=req.body.gender;
-const address=req.body.address;
-const birthdate=req.body.birthdate;
-    const sql=`insert into isharegdata_cust(firstname,lastname,email,password,mobile_number,gender,address,birthdate,customerid)values('${firstname}' , '${lastname}' , '${email}' , '${password}','${mobile_number}','${gender}','${address}','${birthdate}','${customerid}') `
-    con.query(sql,(error,result)=>{
-        if (error) throw error;
-        // console.log("Data is inserted data successfully")
-        // console.log(result)
-    }
-   
-    )
-
-})
-
-
-// const createlogintable =()=>{
-//     sql="create table ishalogincheck (recid bigint(20) ,email varchar(100),password varchar(255))";
-   
-//     con.query(sql,(error,result)=>{
-//         if(error) throw error;
-//         console.log("ishalogincheck table created successfully")
-//         console.log(result)
-//     })
-// }
-
-// createlogintable();
-
 app.post("/logindata",(req,res)=>{
     const loginemail = req.body.loginemail;
     const loginpassword=req.body.loginpassword;
@@ -74,7 +7,8 @@ app.post("/logindata",(req,res)=>{
         if (error){
         }
         else if(result.length==0){
-            res.send("Sorry , user is not exist , your login is fail, please try again")
+            res.send(result)
+            // res.send("Sorry , user is not exist , your login is fail, please try again")
         }
         else if(result[0].accesstoken){
             res.send(result);
@@ -239,8 +173,6 @@ app.post("/managedatainordertable" ,(req,res)=>{
 app.post("/orderdatetable",(req,res)=>{
     const date=req.body.orddate;
     console.log(date)
-    
-    
     sql=`insert into ishaorderlisting(date,orderid) values("${date.slice(0,10)}","${"ORD" + Math.floor(Math.random() * 1000000000)}")`
     con.query(sql,(error,result)=>{
         if(error) throw error;
