a754a09f176a7e7eaf18b6d1591bc851c6c1cc3f
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-04-06 18:30:14 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,3 +1,78 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const jwt=require('jsonwebtoken');
+app.use(cors());
+app.use(express.json());
+
+var mysql =require("mysql");
+var con=mysql.createConnection({
+    host:"192.168.2.8",
+    user:"trainee",
+    password:"trainee@123",
+    database:"trainee"
+})
+
+con.connect(function(error){
+    if(error) throw error;
+    console.log("Database is connected");
+});
+
+// const createtable=()=>{
+//     sql="CREATE TABLE isharegdata_cust ( recid bigint(20),customerid varchar(255),firstname VARCHAR(100), lastname VARCHAR(100) , email varchar(100),password varchar(255), mobile_number integer , gender varchar(50) , address varchar(255) , birthdate varchar(255) ,accesstoken varchar(255))"
+//     con.query(sql,(err,result)=>
+//     {
+//         if (err) throw err;
+//         console.log("your table is created");
+//     })
+// }
+
+// createtable()
+
+
+
+app.post("/regdata",(req,res)=>{
+    console.log(req.body);
+    const customerid= req.body.customerid;
+    const firstname = req.body.firstname;
+    const lastname=req.body.lastname;
+    const email=req.body.email;
+    const password=req.body.password;
+const mobile=req.body.mobile;
+const gender=req.body.gender;
+const address=req.body.address;
+const birthdate=req.body.birthdate;
+
+const sql1 = `select * from isharegdata_cust where email="${email}"`;
+con.query(sql1, (error, result) => {
+  // if(error) throw error;
+  if (result[0] != null) {
+    console.log("email id is already in use");
+    res.send("*Error:Your email id is already in use , please try with another email id")
+  } else {
+    console.log("valid email id");
+    const sql=`insert into isharegdata_cust(firstname,lastname,email,password,mobile_number,gender,address,birthdate,customerid)values('${firstname}' , '${lastname}' , '${email}' , '${password}',${mobile},'${gender}','${address}','${birthdate}','${customerid}')`
+    con.query(sql, (error, result) => {
+      if (error) throw error;
+      console.log("inserted data is successfully");
+    });
+  }
+});
+})
+
+
+// const createlogintable =()=>{
+//     sql="create table ishalogincheck (recid bigint(20) ,email varchar(100),password varchar(255))";
+   
+//     con.query(sql,(error,result)=>{
+//         if(error) throw error;
+//         console.log("ishalogincheck table created successfully")
+//         console.log(result)
+//     })
+// }
+
+// createlogintable();
+
 app.post("/logindata",(req,res)=>{
     const loginemail = req.body.loginemail;
     const loginpassword=req.body.loginpassword;
@@ -45,12 +120,7 @@ app.post("/logindata",(req,res)=>{
                 // console.log(result)
                 res.send(result);
             })
-        }
-
-        
-        
-        
-      
+        }      
     }
     )
 
@@ -64,22 +134,24 @@ app.post("/logindata",(req,res)=>{
 app.post("/update",(req,res)=>{
     const firstname=req.body.firstname;
     const lastname=req.body.lastname;
-    const  email=req.body.email;
+    const email=req.body.email;
     const mobilenumber=req.body.number;
     const gender=req.body.gender;
-   
     const address=req   .body.address;
     const dob=req.body.dob;
+
+
+   
+
     const yysql=`update isharegdata_cust set firstname="${firstname}",lastname="${lastname}" ,email="${email}"  , mobile_number=${mobilenumber},gender="${gender}",address="${address}",birthdate="${dob}" where email="${email}"`;
 con.query(yysql,(error,result)=>{
     if(error) throw error;
     // console.log(yysql)
     // console.log(result)
     res.send(result)
-})
-
-})
-
+});
+}
+)
 
 app.get("/product",(req,res)=>{
     const sql=`select * from ishaproduct_cust`;
@@ -102,7 +174,7 @@ res.send(result)
 })
 
 app.post("/grandtotal",(req,res)=>{
-    const product1=req.body.product1;
+    const product1=req.body.product;
     //  console.log(product1)
      for(let i = 0 ; i < product1.length ; i++){
         // console.log(product1[i].productprice);
@@ -150,7 +222,7 @@ app.get("/totalpriceoforder",(req,res)=>{
 })
 
 app.get("/displayorderlisting",(req,res)=>{
-    sql =`select * from ishaorderlisting`;
+    sql =`select  * from ishaorderlisting group by orderid`;
     con.query(sql,(error,result)=>{
         if(error) throw error;
         console.log(result)
@@ -160,7 +232,7 @@ app.get("/displayorderlisting",(req,res)=>{
 })
 
 app.post("/managedatainordertable" ,(req,res)=>{
-    sql=`truncate table ishaorder`;
+    sql=`truncate table ishaorder `;
     con.query(sql,(error,result)=>{
         if(error) throw error;
         console.log(result)
@@ -172,13 +244,35 @@ app.post("/managedatainordertable" ,(req,res)=>{
 
 app.post("/orderdatetable",(req,res)=>{
     const date=req.body.orddate;
+    const product1=req.body.product1;
+    console.log(product1)
     console.log(date)
-    sql=`insert into ishaorderlisting(date,orderid) values("${date.slice(0,10)}","${"ORD" + Math.floor(Math.random() * 1000000000)}")`
+    var ran = "ORD" + Math.floor(Math.random() * 1000000000) ;
+    console.log('Number',ran) ; 
+    for(let i = 0 ; i < product1.length ; i++){
+        console.log(product1[i].productprice)
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        sql=`insert into ishaorderlisting(date,orderid,ordertime,productcode,productname,productprice,quantity,totalprice) values("${date.slice(0,10)}","${ran}","${time}","${product1[i].productcode}","${product1[i].productname}","${product1[i].productprice}","${product1[i].quantity}","${product1[i].productprice*product1[i].quantity}")`
+        con.query(sql,(error,result)=>{
+            if(error) throw error;
+            console.log(result);
+            console.log(sql);
+            console.log("date in table is added successfully")
+            
+        })
+
+    }
+    res.send(ran)
+})
+
+app.post("/showview",(req,res)=>{
+    const id=req.body.id;
+    const sql = `select * from ishaorderlisting where orderid="${id}"`
     con.query(sql,(error,result)=>{
         if(error) throw error;
-        console.log(result);
-        console.log(sql);
-        console.log("date in table is added successfully")
+        console.log(result)
+        res.send(result)
     })
 })
 
