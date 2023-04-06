6047f4cceb022faef631cded1f98ec0cac7eb1f5
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-04-06 18:31:09 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -5,30 +5,86 @@ import {Link} from 'react-router-dom'
 function Login(){
   const[showlogin,setshowlogin]=useState(true);
   const [showprofile,setshowprofile]=useState(false);
-  const[firstname,setfirstname]=useState("");
-  const [lastname, setlastname] = useState("");
-  const [email, setemail] = useState("");
-  const [mobilenumber, setmobilenumber] = useState("");
-  const [gender, setgender] = useState("");
+  const [showmainpage,setshowmainpage]=useState(false);
+
+  // const[firstname,setfirstname]=useState("");
+  // const [lastname, setlastname] = useState("");
+  // const [email, setemail] = useState("");
+  // const [mobilenumber, setmobilenumber] = useState("");
+  // const [gender, setgender] = useState("");
   const [address, setaddress] = useState("");
   const [dob, setdob] = useState("");
     
-  
+  function phonenumber(inputtxt)
+    {
+      var phoneno = /^\d{10}$/;
+      if(inputtxt.value.match(phoneno))
+            {
+          return true;
+            }
+          else
+            {
+            alert("Please enter valid mobile number");
+            return false;
+            }
+    }
+
+
+    function allLetter(inputtxt)
+       
+    {
+      console.log(inputtxt);
+     var letters = /^[A-Za-z]+$/;
+     if(inputtxt.value.match(letters))
+       {
+        return true;
+       }
+     else
+       {
+       alert("Only characters are allowed in " + inputtxt.name);
+       return false;
+       }
+    }
 
   const updatesubmit=async(e)=>{
+
+
     const form = e.target;
 e.preventDefault();
-console.log("update")
-const res = await axios.post("http://localhost:8006/update",{
+if(allLetter(document.profile.fname)&&allLetter(document.profile.lname)&&phonenumber(document.profile.number)){
+  var today = new Date();
+var dd = today.getDate();
 
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = yyyy+'-'+mm+'-'+dd;
+console.log(today);
+console.log(document.profile.dob)
+console.log(form.dob.value)
+const inputdate=form.dob.value;
+console.log(inputdate)
+if(today>inputdate){
   
-  firstname :form.fname.value ,
+  console.log("right date")
+  document.getElementById("bday").innerHTML="Right birthdate"
+      document.getElementById("bday").style.color="green"
+      console.log("update")
+
+      const res = await axios.post("http://localhost:8006/update",{
+        firstname :form.fname.value ,
 
    lastname : form.lname.value ,
 
    email : form.email.value ,
-
-  
 
    number : form.number.value ,
 
@@ -37,10 +93,26 @@ const res = await axios.post("http://localhost:8006/update",{
    address : form.address.value ,
 
    dob : form.dob.value ,
-})
-.then((res)=>{
+  })
+  // .then((res)=>{
+  //   console.log(res.data)
+  // })
   console.log(res.data)
-})
+   
+    }else{
+      console.log("wrong date")
+      document.getElementById("bday").innerHTML="Wrong birthdate"
+      document.getElementById("bday").style.color="red"
+}
+}
+
+
+
+
+  
+  
+
+
   }
   
   
@@ -50,7 +122,6 @@ const res = await axios.post("http://localhost:8006/update",{
 
     const clickloginaction=async(e)=>{
         e.preventDefault();
-
         if(loginpassword.length < 8) { 
           return (alert("Error: Password must be at least 8 characters"))
                } else if(loginpassword.search(/[a-z]/) < 0) { 
@@ -74,14 +145,21 @@ const res = await axios.post("http://localhost:8006/update",{
                     loginpassword : loginpassword
                  })
                .then((res)=>{
-              
-                console.log(res.data)
+              console.log("this is data")
                 console.log(res.data.length)
                 if(res.data.length==0){
                   console.log("Sorry , user is not exist , your login is fail, please try again")
                   setdata([""]);
+                  document.getElementById("loginerror").innerHTML="Sorry , user is not exist , your login is fail, please try again"
+                  document.getElementById("loginerror").style.color="red"
                 }else{
                   console.log("Login is Successfull")
+                  setshowmainpage(true)
+                  setshowlogin(false)
+                  setdata(res.data[0])
+                  document.getElementById("loginerror").innerHTML="Login is Successfull"
+                  document.getElementById("loginerror").style.color="green"
+
                 }
                 //  if(res.data == "Sorry , user is not exist , your login is fail, please try again"){
                 //    console.log(res.data);
@@ -98,7 +176,7 @@ const res = await axios.post("http://localhost:8006/update",{
                })
 
                 }
-              
+         
         
 
     }
@@ -122,10 +200,12 @@ const res = await axios.post("http://localhost:8006/update",{
           onChange={(e)=>{setloginemail(e.target.value)}}
         /></div>
 
+
 <div align="center" class="title">
       <label htmlFor="">Password</label>
+
         <input
-          type="text"
+          type="password"
           name='password'
           placeholder="Please enter your password"
           class="form-control"
@@ -139,6 +219,11 @@ const res = await axios.post("http://localhost:8006/update",{
           {/* </Link> */}
 
       </div>
+
+<br />
+<div>
+  <p id="loginerror"></p>
+</div>
       <div className='isha'>
 
 <label>Not registered yet?</label>
@@ -146,20 +231,24 @@ const res = await axios.post("http://localhost:8006/update",{
 {<Link to="/registration" >Please click here to register </Link>}
 </div>
 <br />
-<div><button class="btn btn-success" onClick={()=>{setshowprofile(true);
-setshowlogin(false)}}>Please click here to see your profile</button></div>
- <tr className='apptable'>
-             
-              <Link to="/product">Product Screen</Link>
-              </tr>
+
+ 
 
 </form>
         </div>
         :null}
+
+{showmainpage?<><div><button class="btn btn-success" onClick={()=>{setshowprofile(true);
+setshowlogin(false)}}>Please click here to see your profile</button></div>
+<tr className='apptable'>
+             
+             <Link to="/product">Product Screen</Link>
+             </tr></>:null}
+
         {showprofile?
         <div className='regdiv'>
 
-            <form className='f1' onSubmit={updatesubmit} >
+            <form className='f1' onSubmit={updatesubmit} name="profile" >
                 <div align="center" class="title">
 
                     <h1>Profile Page</h1>
@@ -246,7 +335,7 @@ name='email'
                         name='address'
                         class="form-control"
                         defaultValue={data.address}
-                        onChange={(e)=>{setaddress(e.target.value)}}
+                        // onChange={(e)=>{setaddress(e.target.value)}}
                         required
 
                     />
@@ -260,11 +349,11 @@ name='email'
                         required
                         name='dob'
                         defaultValue={data.birthdate}
-                        onChange={(e)=>{setdob(e.target.value)}}
+                        // onChange={(e)=>{setdob(e.target.value)}}
 
                     />
                 </div>
-
+<p id="bday"></p>
 
                 <div>
                     <button type="submit" class="btn btn-primary" >Submit</button>
